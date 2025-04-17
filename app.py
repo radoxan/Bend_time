059fce509bfff042ac0c93b8186eeec5dcc6cd73
Radek Krupa <radekkrupa96@gmail.com>
2025-04-17 13:02:39 +0200

    Dodano przycisk

--- app.py
+++ app.py
@@ -2,9 +2,17 @@ import streamlit as st
 import pycaret.regression
 import pandas as pd
 
-def predict(model_name, df):
+def predict(capacity, weigth, width, length, bends):
+    df = pd.DataFrame({
+    'Capacity': [capacity],
+    'Weigth': [weigth],
+    'Width': [width],
+    'Length': [length],
+    'Bends': [bends]
+     })
+    model_name = pycaret.regression.load_model('bending_time_regression_compress')
     predictions = pycaret.regression.predict_model(model_name, data=df)
-    time_in_minutes = float(predictions['prediction_label'].iloc[0])    
+    time_in_minutes = float(predictions['prediction_label'].iloc[0])
     hours = int(time_in_minutes // 60)
     minutes = int(time_in_minutes % 60)
     formatted_minutes = f"{minutes:02}"
@@ -12,7 +20,7 @@ def predict(model_name, df):
     formatted_seconds = f"{seconds:02}"
     st.header(f'Czas na wykonanie elementów to: {hours}:{formatted_minutes}:{formatted_seconds}')
 
-st.title("Oblicz czas wykonania zlecenia.")
+st.title("Oblicz czas wykonania zlecenia")
 
 length = st.number_input(label="Podaj długość")
 width = st.number_input(label="Podaj rozwinięcie")
@@ -20,14 +28,5 @@ weigth = st.number_input(label="Podaj wagę")
 bends = st.number_input(label="Ilość gięć")
 capacity = st.number_input(label="Ilość sztuk")
 
-df_2 = pd.DataFrame({
-    'Capacity': [capacity],
-    'Weigth': [weigth],
-    'Width': [width],
-    'Length': [length],
-    'Bends': [bends]
-})
-
-
-model = pycaret.regression.load_model('bending_time_regression_compress')
-predict(model, df_2)
+if st.button(label="Oblicz"):
+    predict(capacity, weigth, width, length, bends)
