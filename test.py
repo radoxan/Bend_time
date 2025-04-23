f1a6f3263ca44913341a3ce4bed829378fdb4e13
Radek Krupa <radekkrupa96@gmail.com>
2025-04-23 07:39:16 +0200

    Dodano tryb wide do aplikacji streamlit

--- test.py
+++ test.py
@@ -4,14 +4,26 @@ import pandas as pd
 import time
 from datetime import datetime
 
+
+st.set_page_config(layout="wide")
+
+
 # Inicjalizacja session_state
 if 'choosen_parts' not in st.session_state:
-    st.session_state['choosen_parts'] = []
+    st.session_state['choosen_parts'] = pd.DataFrame()
+
+if 'new_df' not in st.session_state:
+    st.session_state['new_df'] = pd.DataFrame()
 
 if 'input_name' not in st.session_state:
     st.session_state['input_name'] = ''
 if 'name' not in st.session_state:
     st.session_state['name'] = ''
+
+if 'input_index' not in st.session_state:
+    st.session_state['input_index'] = ''
+if 'index' not in st.session_state:
+    st.session_state['index'] = ''
 
 if 'input_length' not in st.session_state:
     st.session_state['input_length'] = 0
@@ -39,7 +51,7 @@ if 'capacity' not in st.session_state:
     st.session_state['capacity'] = 0
 
 def predict(capacity, weigth, width, length, bends):
-    df = pd.DataFrame({
+    st.session_state['new_df'] = pd.DataFrame({
     'Capacity': [capacity],
     'Weigth': [weigth],
     'Width': [width],
@@ -47,14 +59,18 @@ def predict(capacity, weigth, width, length, bends):
     'Bends': [bends]
      })
     model_name = pycaret.regression.load_model('bending_time_regression_compress')
-    predictions = pycaret.regression.predict_model(model_name, data=df)
+    predictions = pycaret.regression.predict_model(model_name, data=st.session_state['new_df'])
     time_in_minutes = float(predictions['prediction_label'].iloc[0])
     hours = int(time_in_minutes // 60)
     minutes = int(time_in_minutes % 60)
     formatted_minutes = f"{minutes:02}"
     seconds = int((time_in_minutes - hours * 60 - minutes) * 60)
     formatted_seconds = f"{seconds:02}"
-    st.header(f'Czas na wykonanie elementów to: {hours}:{formatted_minutes}:{formatted_seconds}')
+    predicted_time = f'{hours}:{formatted_minutes}:{formatted_seconds}'
+    with st.spinner(text='Liczę czas wykonania...'):
+        time.sleep(3)
+    st.header(f'Czas na wykonanie elementów to: {predicted_time}')
+    st.session_state['new_df']['Time'] = predicted_time
 
 def przechwyc_wartosci():
     st.session_state['input_name'] = str(st.session_state['name'])
@@ -63,6 +79,15 @@ def przechwyc_wartosci():
     st.session_state['input_weight'] = float(st.session_state['weight'])
     st.session_state['input_bends'] = int(st.session_state['bends'])
     st.session_state['input_capacity'] = int(st.session_state['capacity'])
+    st.session_state['input_index'] = str(st.session_state['index'])
+
+def dodaj_do_kolejki():
+    st.session_state['new_df']['Index'] = st.session_state['input_index']
+    st.session_state['choosen_parts'] = pd.concat([st.session_state['choosen_parts'],st.session_state['new_df']])
+
+def czysc_tabele():
+    clear_df = pd.DataFrame()
+    st.session_state['choosen_parts'] = clear_df
 
 # Wczytanie danych
 df_1 = pd.read_excel('Bend_parts_data.xlsx')
@@ -76,6 +101,7 @@ parts_df = parts_df.dropna(subset=['Numer'])
 # Streamlit input and selection
 col1, col2 = st.columns(2)
 with col1:
+    st.title('Wyszukaj plik:')
     options = parts_df['Numer']
     search_number = st.text_input('Wprowadź numer do wyszukania')
     wyniki = parts_df[parts_df['Numer'].str.startswith(search_number)]['Numer'].tolist()
@@ -88,6 +114,7 @@ with col1:
     st.session_state['capacity'] = capacity    
     st.write(f'Ilość elementów do wykonania: {capacity}')
     st.session_state['name'] = choosen_row['Nazwa'].astype(str).iloc[0]
+    st.session_state['index'] = choosen_row['Numer'].astype(str).iloc[0]
     st.session_state['weight'] = choosen_row['Masa'].astype(float).iloc[0]
     st.session_state['width'] = choosen_row['Rozwinięcie'].iloc[0]
     st.session_state['length'] = choosen_row['Długość'].astype(float).iloc[0]
@@ -95,23 +122,30 @@ with col1:
     choosen_row['Ilość'] = st.session_state['capacity']
     st.button(label="Wczytaj dane wybranego detalu", on_click=przechwyc_wartosci)
 
+    
+    st.markdown('<hr>', unsafe_allow_html=True)
+    
+    st.title('Stwórz tabelę:')
+    st.dataframe(st.session_state['choosen_parts'])
+    st.button(label='Wyczyść tabelę', on_click=czysc_tabele)
+
 with col2:
+    st.title("Wprowadź dane:")
     name = st.text_input(label="Podaj nazwę", value=st.session_state['input_name'])
+    index = st.text_input(label="Podaj numer rysunku", value=st.session_state['input_index'])
     length = st.number_input(label="Podaj długość", max_value=float(6100), min_value=float(0), value=float(st.session_state['input_length']), step=float(1))
     width = st.number_input(label="Podaj rozwinięcie", max_value=float(3000), min_value=float(0), value=float(st.session_state['input_width']), step=float(1))
     weigth = st.number_input(label="Podaj wagę", min_value=float(0), max_value=float(100), step=float(1), value=float(st.session_state['input_weight']))
     bends = st.number_input(label="Ilość gięć", step=int(1), min_value=int(0), max_value=int(12), value=int(st.session_state['bends']))
     capacity = st.number_input(label="Ilość sztuk", step=int(1), min_value=int(0), value=int(st.session_state['capacity']))
 
-    df = pd.DataFrame({
+    st.session_state['new_df'] = pd.DataFrame({
         'Capacity': [capacity],
         'Weigth': [weigth],
         'Width': [width],
         'Length': [length],
         'Bends': [bends]
     })
-
-    st.dataframe(df)
 
     if st.button(label="Oblicz"):
         if length == 0:
@@ -126,4 +160,5 @@ with col2:
             st.header('Podaj ilość sztuk')
         else:
             predict(capacity, weigth, width, length, bends)
-            time.sleep(5)
+
+    st.button(label='Dodaj do kolejki', on_click= dodaj_do_kolejki)
